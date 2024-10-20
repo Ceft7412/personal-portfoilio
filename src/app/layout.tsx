f914c2502450cacf49bb92e563d893853d08184d
Ceft <146732550+Ceft7412@users.noreply.github.com>
2024-10-20 18:00:38 +0800

    Replaced defaults.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="min-w-80">{children}</main>
       </body>
     </html>
   );
