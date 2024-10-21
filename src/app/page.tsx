3fc2dd57a2ff68f384313537a8e6b6709caf9f46
Ceft <146732550+Ceft7412@users.noreply.github.com>
2024-10-21 21:45:42 +0800

    Using the 3 components (sections) as different function instead of inserting them here.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import { information } from "../resources/basicInfo";
+"use client";
 
-export default function Home() {
+import About from "@/components/About";
+import Home from "@/components/Home";
+import Projects from "@/components/Projects";
+
+export default function Page() {
   return (
-    <section className="bg-[#273316] text-[#f2a156] min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-start md:items-center">
-        <span className="">HELLO, IM</span>
-        <h1 className="font-bold text-[30px]">
-          {information.fullName.toUpperCase()}
-        </h1>
-      </div>
-    </section>
+    <>
+      <Home />
+      <About />
+      <Projects />
+    </>
   );
 }
