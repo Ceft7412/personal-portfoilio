172d93173b72ec9b7785a783d4346edb11ee60eb
Ceft <146732550+Ceft7412@users.noreply.github.com>
2024-10-21 21:46:43 +0800

    Created Projects section and its background color.

---
+++ src/components/Projects.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <section
+      id="projects"
+      className="bg-[#233D56] text-[#A5D9EE] p-2 py-10 sm:p-10 flex flex-col items-center gap-10"
+    >
+      <h1 className="text-[40px] font-bold sm:text-[35px] font-interphase">
+        PROJECTS
+      </h1>
+      <span>Projects</span>
+    </section>
+  );
+}
